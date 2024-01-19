23717a093e2bb1c33633e2802f1132bcd3563984
DogyeomPark <qkrehfud@gmail.com>
2024-01-19 18:41:25 +0900

    2024-01-19
    퀘스트 + HUD, HUD 연동 테스트

--- Assets/01_Scripts/J/Player.cs
+++ Assets/01_Scripts/J/Player.cs
@@ -32,7 +32,7 @@ public class Player : MonoBehaviour
     public TimelineAsset[] Ta;
     Boss boss;
     TPScontroller tps;
-    StateManager stateManager;
+    public StateManager stateManager;
     MeshRenderTail meshRenderTail;
    
 
