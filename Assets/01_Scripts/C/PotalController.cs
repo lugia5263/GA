fef64a61efbe1228d386cab747fbe67258f32b10
chlrjsdyd <156290752+chlrjsdyd@users.noreply.github.com>
2024-01-23 19:13:52 +0900

    0123
    퇴근 전 마을 추가

--- Assets/01_Scripts/C/PotalController.cs
+++ Assets/01_Scripts/C/PotalController.cs
@@ -1,39 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 public class PotalController : MonoBehaviour
 {
-    public bool isDunMenu = false;
+    public bool isPotal = false;
     public GameObject potalPanel;
+
+    public GameObject bigPortal;
+
+    private void Awake()
+    {
+        bigPortal = GameObject.Find("Portal red");
+        bigPortal.SetActive(false);
+    }
+    void Start()
+    {
+        potalPanel = GameObject.Find("PortalPanel");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            UnitSetBtn();
+            Potal();
+            bigPortal.SetActive(true);
+            bigPortal.GetComponent<Jun_TweenRuntime>().Play();
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            UnitSetBtn();
+            bigPortal.SetActive(false);
+            Potal();
         }
     }
-    public void UnitSetBtn()
+    public void Potal()
     {
-        if (isDunMenu == false)
+        if (isPotal == false)
         {
             Jun_TweenRuntime[] gameObjects = potalPanel.GetComponents<Jun_TweenRuntime>();
             gameObjects[0].Play();
-            isDunMenu = true;
+            isPotal = true;
         }
         else
         {
             Jun_TweenRuntime[] gameObjects = potalPanel.GetComponents<Jun_TweenRuntime>();
             gameObjects[1].Play();
-            isDunMenu = false;
+            isPotal = false;
         }
     }
 }
