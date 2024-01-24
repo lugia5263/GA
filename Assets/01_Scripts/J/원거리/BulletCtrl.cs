b99dcbf0c05490ab548a60eb0daf7076464a4649
whangus <whangus000@gmail.com>
2024-01-24 20:57:35 +0900

    .0.
    ..

--- Assets/01_Scripts/J/원거리/BulletCtrl.cs
+++ Assets/01_Scripts/J/원거리/BulletCtrl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletCtrl1 : MonoBehaviour
+public class BulletCtrl : MonoBehaviour
 {
    
     void Start()
