22dc83b3947971f6cbb78f6d24d46c7a3a58956a
LHyunChang <ckdqwqw@naver.com>
2024-01-26 03:14:57 +0900

    0126
    집에서한거

--- Assets/01_Scripts/L/SpawnScipt.cs
+++ Assets/01_Scripts/L/SpawnScipt.cs
@@ -7,17 +7,18 @@ using Photon.Realtime;
 public class SpawnScipt : MonoBehaviourPunCallbacks
 {
     public GameObject[] characterPrefabs;
-
+    int curSlotNum = SelectSlot.slotNum;
     public void CreatePlayer()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
-
+        
         if (PhotonNetwork.IsConnected)
         {
             Transform[] points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
             int idx = Random.Range(1, points.Length);
 
-            PhotonNetwork.Instantiate(characterPrefabs[(int)DataMgr.instance.currentCharacter].name, points[idx].position, points[idx].rotation, 0);
+            //PhotonNetwork.Instantiate(characterPrefabs[(int)DataMgr.instance.currentCharacter].name, points[idx].position, points[idx].rotation, 0);
+            PhotonNetwork.Instantiate(characterPrefabs[curSlotNum].name, points[idx].position, points[idx].rotation, 0);
         }
     }
 }
